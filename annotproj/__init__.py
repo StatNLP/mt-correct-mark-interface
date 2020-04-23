97331628cabd213ceef83129ef4f1be1223d4f7c
Nathan Berger <berger1954@gmail.com>
2020-04-23 13:45:41 +0200

    removed hardcoded secret key to read env var

--- annotproj/__init__.py
+++ annotproj/__init__.py
@@ -1,11 +1,14 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5ed5583433f8c71a20494a017de91291'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "my default secret key")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
